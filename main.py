8c7a65ea6662147edb76ba6293023e70b78f4aa6
Anton <antongustafsson.17@gmail.com>
2025-12-16 00:29:48 +0100

    Add ski book system functionality
    Add the ability to enter amount of travelers and their age category

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from db_client import AgeCategory, DatabaseClient, PriceEntry, SkiCategory
 
 
 def print_menu():
+    print("\nPlease enter the number of your choice\n")
     print("Menu:")
     print("\"1\" list prices")
     print("\"2\" create booking")
-    print("\"0\" exit")
+    print("\"0\" exit\n")
 
 
 def print_price_list(
@@ -38,8 +39,80 @@ def print_price_list(
         print("\n\n")
 
 
-def print_booking_menu():
-    print("Booking menu is not yet implemented.")
+def print_age_categories(ageCategories: dict[int, AgeCategory]):
+    print("Age Categories:")
+    for agecatid, agecat in ageCategories.items():
+        print(f"{agecatid}: {agecat.name} ({agecat.minage}-{agecat.maxage})")
+    print("\n")
+
+
+def wait_for_user_input(prompt: str) -> str:
+    print(prompt)
+    print("You can type 'exit' or 'quit' to leave.")
+    while True:
+        try:
+            line = input('> ').strip()
+        except EOFError:
+            print()
+            break
+
+        if not line:
+            continue
+        elif line.lower() in ('exit', 'quit'):
+            print('Exiting booking menu.')
+            return None
+
+        return line
+
+
+def ask_for_amount_of_travelers() -> int | None:
+    prompt = "Please enter the amount of travelers:"
+    line = wait_for_user_input(prompt)
+
+    if line is None:
+        return None
+    elif line.isdigit() and int(line) > 0:
+        print(f"\nNumber of travelers: {int(line)}\n")
+        return int(line)
+    else:
+        print(f"Invalid input: {line}, please enter a listed number")
+        return ask_for_amount_of_travelers()
+
+
+def ask_for_age_category(ageCategories: dict[int, AgeCategory]) -> int | None:
+    print_age_categories(ageCategories)
+    prompt = "Please enter the age category ID from the list above:"
+    line = wait_for_user_input(prompt)
+
+    if line is None:
+        return None
+    elif line.isdigit() and int(line) > 0:
+        return int(line)
+    else:
+        print(f"Invalid input: {line}, please enter a listed number")
+        return ask_for_age_category(ageCategories)
+
+
+def print_booking_menu(
+        prices: list[PriceEntry],
+        ageCategories: dict[int, AgeCategory],
+        skiCategories: dict[int, SkiCategory],
+    ) -> None:
+    travelers = ask_for_amount_of_travelers()
+
+    for traveler in range(1, travelers+1):
+        print(f"Traveler {traveler}/{travelers}:")
+        age_category = ask_for_age_category(ageCategories)
+        if not age_category:
+            return
+        print(f"Traveler {traveler} is {ageCategories[age_category].name}\n")
+
+    # save traveler info
+    # list the ski categories
+    # please enter ski category
+
+    # show total price
+    print("mumma vilken smaskig bokning")
 
 
 def main():
@@ -49,7 +122,6 @@ def main():
     skiCategories = db_client.fetch_ski_categories()
     db_client.close()
 
-    print("Please enter the number of your choice or \"help\" for assistance")
     print_menu()
     while True:
         try:
@@ -71,7 +143,7 @@ def main():
         elif cmd == '1':
             print_price_list(prices, ageCategories, skiCategories)
         elif cmd == '2':
-            print_booking_menu()
+            print_booking_menu(prices, ageCategories, skiCategories)
         else:
             print(f"Unknown command: {cmd}.")
 
