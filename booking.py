61492c84856146e1a8bdf8d9551507fc9b3a1cf8
Anton <antongustafsson.17@gmail.com>
2025-12-21 13:44:28 +0100

    Refactor

--- booking.py
+++ booking.py
@@ -2,7 +2,7 @@ from clients.db_client import AgeCategory, DatabaseClient, PriceEntry, SkiCatego
 from clients.yr_client import YRClient
 from front_end import list_prices, print_age_categories, print_divider, print_ski_categories
 from pydantic import BaseModel
-from utility.economy import get_price_reduction
+from utility.economy import calculate_traveler_price
 from utility.exceptions import AbortBookingException, AbortException
 from user_input import ask_for_age, ask_for_amount_of_travelers, ask_for_ski_category
 
@@ -18,38 +18,6 @@ class TravelerBooking(BaseModel):
 class Booking(BaseModel):
     traveler_bookings: list[TravelerBooking]
     total_price: int
-
-
-def calculate_traveler_price(
-    prices: list[PriceEntry],
-    age_category_id: int,
-    ski_category_id: int,
-    age_categories: dict[int, AgeCategory],
-) -> tuple[int, bool]:
-    yr_client = YRClient()
-    current_weather = yr_client.get_current_weather()
-
-    if current_weather:
-        reduction = 1
-
-    for record in prices:
-        if record.agecatid == age_category_id and record.skiid == ski_category_id:
-            if current_weather:
-                reduction = get_price_reduction(age_category_id,
-                                                age_categories,
-                                                current_weather.data.air_temperature
-                                                )
-            else:
-                reduction = 1
-
-            price = round(record.price * reduction)
-            price_reduced = record.price - price
-            return price, price_reduced
-
-    raise ValueError(
-        f"Couldn't find prices for age category with id {age_category_id} " \
-        f"and ski category with id {ski_category_id}"
-    )
 
 
 def calculate_booking_price(
@@ -84,7 +52,14 @@ def create_booking(
         ski_category_id = ask_for_ski_category(ski_categories)
 
         try:
-            price, price_reduced = calculate_traveler_price(prices, age_category_id, ski_category_id, age_categories)
+            yr_client = YRClient()
+            weather = yr_client.get_current_weather()
+            price, price_reduced = calculate_traveler_price(prices,
+                                                            age_category_id,
+                                                            ski_category_id,
+                                                            age_categories,
+                                                            weather.data.air_temperature
+                                                            )
         except ValueError as e:
             print(repr(e))
             print("Using default price 0")
